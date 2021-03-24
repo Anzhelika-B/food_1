672fd344ce938ba21c1f224bce37ca350ed45d22
Anzhelika-B <ab990393@gmail.com>
2021-03-24 13:27:09 +0200

    secon commit footer-22

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 function Footer() {
-    return (  
-        <footer className="page-footer #c0ca33 lime darken-1 ">
-              
-            <div className="footer-copyright">
-                <div className="container">
-                    © { new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#!">***</a>
+    return (
+        <footer className='page-footer #c0ca33 lime darken-1 '>
+            <div className='footer-copyright'>
+                <div className='container'>
+                    © {new Date().getFullYear()} Copyright Text -22
+                    <a className='grey-text text-lighten-4 right' href='#!'>
+                        ***
+                    </a>
                 </div>
             </div>
-
         </footer>
-   );
+    );
 }
 
 export default Footer;
